95b91310ef5680114241b933b735d9e0f775dec8
Xochitl Perez <xochitlperez@macbook-pro-de-xochitl-perez.local>
2011-10-18 22:59:20 -0500

    Initial Commit

---
+++ BeerSong/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  BeerSong
+//
+//  Created by Xochitl Perez on 18/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+

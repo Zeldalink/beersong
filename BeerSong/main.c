89106f17171780562c3e406684266cd502a68204
Xochitl <xochitl.zeldalink@gmail.com>
2012-07-02 17:47:44 -0500

    //newchallenge

--- BeerSong/main.c
+++ BeerSong/main.c
@@ -7,12 +7,22 @@
 //
 
 #include <stdio.h>
+void singTheSong(int numberOfBottles){
+    if(numberOfBottles==0){
+        printf("There are simply no more bottles of beer on the wall.\n");
+    }
+    else{
+        printf("%d bottles of beer in the wall. %d bottles of beer.\n",numberOfBottles, numberOfBottles );
+        int oneFewer =numberOfBottles-1;
+        printf("Take one down, pass it around, %d bottles of beert on the wall.\n",oneFewer);
+        singTheSong(oneFewer);//this function is call itself!!!
+        printf("Put a bottle in the recycling, %d empety bottles in the bin.\n", numberOfBottles);
+    }
+}
 
 int main (int argc, const char * argv[])
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
+    singTheSong(99);
     return 0;
 }
 
